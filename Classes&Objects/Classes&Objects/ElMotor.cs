de2dbb57356048b65c92f7c46b46b7f60832c9fb
maxvyshenskyi <id0965582756@gmail.com>
2025-09-04 00:28:00 +0300

    Add files via upload
    Конструктор класу, екземпляри класу та їх виведення на екран.

---
+++ Classes&Objects/Classes&Objects/ElMotor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Objects
+{
+    internal class ElMotor
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Power { get; set; }
+        public int PhasesAmount { get; set; }
+        public int Weight { get; set; }
+        private static List<ElMotor> elMotors = new List<ElMotor>();
+
+        public ElMotor(string Name, string Type, int Power, int PhasesAmount, int Weight)
+        {
+            this.Name = Name;
+            this.Type = Type;
+            this.Power = Power;
+            this.PhasesAmount = PhasesAmount;
+            this.Weight = Weight;
+            elMotors.Add(this);
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Характеристики моделі {Name}:");
+            Console.WriteLine($"Тип двигуна: {Type}");
+            Console.WriteLine($"Потужнісь двигуна: {Power} кВт");
+            Console.WriteLine($"Кількість фаз двигуна: {PhasesAmount}");
+            Console.WriteLine($"Маса двигуна: {Weight} кг");
+            Console.WriteLine("");
+        }
+        public void ShowName()
+        {
+            Console.WriteLine($"{Name}");
+        }
+        public static void ShowList()
+        {
+            Console.WriteLine("Список збережених моделей двигунів:");
+            foreach (var ElMotor in elMotors)
+            {
+                ElMotor.ShowName();
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ Classes&Objects/Classes&Objects/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            ElMotor emotor1 = new ElMotor("1PH7137-2NG03-0BC0", "Синхронний", 28, 3, 130);
+            ElMotor emotor2 = new ElMotor("АІР 100 L4", "Асинхронний", 4, 3, 39);
+
+            ElMotor.ShowList();
+            emotor1.ShowInfo();
+            emotor2.ShowInfo();
+        }
+    }
+}
