908f102d47ccd0df54ac96980037e690c0f6f431
maxvyshenskyi <id0965582756@gmail.com>
2025-09-24 00:54:10 +0300

    Add files via upload
    Робота з класами та об’єктами.

---
+++ lab1oop/lab1oop/EMotor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1oop
+{
+    internal class EMotor
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Power { get; set; }
+        public int Phases { get; set; }
+        public double Mass { get; set; }
+
+        public EMotor(string name, string type, int power, int phases, double mass)
+        {
+            Name = name;
+            Type = type;
+            Power = power;
+            Phases = phases;
+            Mass = mass;
+        }
+
+        public string GetInfo()
+        {
+            return $"Двигун {Name}\n" +
+                   $"Тип: {Type}\n" +
+                   $"Потужність: {Power} кВт\n" +
+                   $"Фази: {Phases}\n" +
+                   $"Маса: {Mass} кг";
+        }
+    }
+}
