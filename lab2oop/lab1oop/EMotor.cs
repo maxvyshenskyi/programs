9ae5a4f83365d19858c053d9fe4c33508a2a4dd6
maxvyshenskyi <id0965582756@gmail.com>
2025-10-01 23:47:57 +0300

    Add files via upload

---
+++ lab2oop/lab1oop/EMotor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1oop
+{
+    internal class EMotor
+    {
+        public string Name { get; set; }
+        private string Type { get; set; }
+        private int Power { get; set; }
+        public int Phases { get; set; }
+        public double Mass { get; set; }
+
+        public EMotor(string name, string type, int power, int phases, double mass)
+        {
+            Name = name;
+            Type = type;
+            Power = power;
+            Phases = phases;
+            Mass = mass;
+        }
+
+        public string GetName()
+            { return $"Назва: {Name}\n"; }
+        public string GetType()
+            { return $"Тип: {Type}\n"; }
+        public string GetPower()
+        { return $"Потужність: {Power} кВт\n"; }
+        public string GetPhases()
+        { return $"К-сть фаз: {Phases}\n"; }
+        public string GetMass()
+        { return $"Вага: {Mass} кг\n"; }
+
+        public void SetType(string type)
+        { 
+            Type = type;
+        }
+        public void SetPower(int power)
+        {
+            Power = power;
+        }
+
+        public override string ToString()
+        {
+            return $"Двигун {Name}\n" +
+                   $"Тип: {Type}\n" +
+                   $"Потужність: {Power} кВт\n" +
+                   $"К-сть фаз: {Phases}\n" +
+                   $"Маса: {Mass} кг\n";
+        }
+    }
+}
---
+++ lab2oop/lab1oop/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1oop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(Main.Motors.Select(m => m.Name).ToArray());
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selectedMotor = Main.Motors.FirstOrDefault(m => m.Name == comboBox1.SelectedItem.ToString());
+
+            if (selectedMotor != null)
+            {
+                label1.Text = selectedMotor.GetName();
+                label2.Text = selectedMotor.GetType();
+                label3.Text = selectedMotor.GetPower();
+                label4.Text = selectedMotor.GetPhases();
+                label5.Text = selectedMotor.GetMass();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var selectedMotor = Main.Motors.FirstOrDefault(m => m.Name == comboBox1.SelectedItem.ToString());
+
+            if (selectedMotor != null)
+            { 
+                selectedMotor.SetType(textBox1.Text);
+     
+                MessageBox.Show("Успішно змінено! Нові значення:\n" + " \n" + selectedMotor.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var selectedMotor = Main.Motors.FirstOrDefault(m => m.Name == comboBox1.SelectedItem.ToString());
+
+            if (selectedMotor != null)
+            {
+                int getpower = int.Parse(textBox2.Text);
+                selectedMotor.SetPower(getpower);
+
+                MessageBox.Show("Успішно змінено! Нові значення:\n" + " \n" + selectedMotor.ToString());
+            }
+        }
+    }
+}
