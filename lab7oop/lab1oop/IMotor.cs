9bf315ccc142790574703635e5e32dd48b0e6a14
maxvyshenskyi <id0965582756@gmail.com>
2025-12-01 04:29:39 +0200

    Add files via upload

---
+++ lab7oop/lab1oop/IMotor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1oop
+{
+    public interface IMotor
+    {
+        string Name { get; set; }
+        string Type { get; set; }
+        int Power { get; set; }
+        int Phases { get; set; }
+        double Mass { get; set; }
+        int Voltage { get; set; }
+        int Efficiency { get; set; }
+
+        string GetMotorName();
+        void IncMass(int increment);
+        void ScaleEfficiency(ref int factor);
+    }
+}
